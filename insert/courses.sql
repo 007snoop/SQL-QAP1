29571da0df66af8e6b39dbccd0346f159ca942f9
Colin Yetman <colinguitarist@gmail.com>
2025-02-05 20:06:38 -0330

    added courses scripts, then added enrollment scripts which depend on courses and students

---
+++ insert/courses.sql
@@ -0,0 +1,12 @@
+/* Creates the data for the courses table */
+
+insert into courses (course_name, course_description, professor_id)
+values (
+'Physics 101', 'Physics class lesson plan', 4
+), (
+'Python', 'Intro to python programming', 3
+), (
+'Kotlin', 'Transfer from java', 1
+), (
+'Javascipt', 'Fullstack Javascript', 2
+);
