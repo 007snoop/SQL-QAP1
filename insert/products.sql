ba87f01f4375f9f3be0ac0926cefe3832c5c264b
Colin Yetman <colinguitarist@gmail.com>
2025-02-06 20:24:39 -0330

    product insert script built

---
+++ insert/products.sql
@@ -0,0 +1,13 @@
+/* inserts the data provided into the products table */
+
+INSERT INTO products (product_name, price, stock_quantity)
+    VALUES ('apple', 0.79, 10),
+        ('orange', 0.99, 25),
+        ('pear', 0.99, 10),
+        ('banana', 2.99, 20),
+        ('strawberry', 3.19, 13);
+
+/* reqs:
+
+at least 5 products
+ */
