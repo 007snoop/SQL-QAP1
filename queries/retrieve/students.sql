5c27a16983f12b8dd26588ff2b13f352f94035cb
Colin Yetman <colinguitarist@gmail.com>
2025-02-06 19:23:59 -0330

    built products table

--- queries/retrieve/students.sql
+++
@@ -1,9 +0,0 @@
-/* This Script feeds back the student table with full name */
-
-select
-concat (first_name, ' ', last_name) full_name, 
-email as email,
-school_enrollment_date as date
-from
-students;
-
--- queries/update/professors.sql
+++
@@ -1,5 +0,0 @@
-update professors 
-set department = 'Physics 101'
-where id = 4;
-/* set for task 1 
- i know annie deosnt teach physics */
