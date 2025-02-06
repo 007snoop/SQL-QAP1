b26125573a3291e585c7d51a8334fc678f0e1ad4
Colin Yetman <colinguitarist@gmail.com>
2025-02-06 20:04:42 -0330

    build scripts to insert values into customers table

---
+++ insert/customers.sql
@@ -0,0 +1,11 @@
+/* adds the first_name, last_name, and email to the customers table */
+INSERT INTO customers (first_name, last_name, email)
+values ('Kenzie', 'Moore', 'KM@mail.com'),
+('Colin', 'Grant', 'Grant@mail.com'),
+('Matt', 'English', 'ME@mail.com'),
+('Paul', 'Harrold', 'whatsAnEmail@idk.com');
+
+/* reqs: 
+
+Atleast 4 customers
+ */
