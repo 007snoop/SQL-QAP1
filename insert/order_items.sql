73d1fbbc942d159203ef99ba2c4952882780e03d
Colin Yetman <colinguitarist@gmail.com>
2025-02-06 20:55:58 -0330

    retieve stock quantity and name script added

---
+++ insert/order_items.sql
@@ -0,0 +1,6 @@
+INSERT INTO order_items (order_id, product_id, quantity)
+VALUES (1, 5, 2),
+(2, 1, 5),
+(3, 3, 10),
+(4, 2, 8),
+(5, 4, 6);
---
+++ queries/retrieve/StockNameQuantity.sql
@@ -0,0 +1,8 @@
+/* displays product name and stock quantity from products table */
+
+SELECT product_name, stock_quantity FROM products;
+
+/* 
+reqs:
+Retrieve the names and stock quantities of all products.
+ */
